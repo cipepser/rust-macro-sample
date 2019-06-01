c86dbe10d2ccc950122a1f795fa64d589e70fdfb
cipepser <respepic@gmail.com>
2019-06-01 18:15:00 +0900

    add define_fn

--- src/main.rs
+++ src/main.rs
@@ -21,3 +21,18 @@ fn test_patterns() {
     assert_eq!(patterns!(dog), "dog");
     assert_eq!(patterns!({}), "brace");
 }
+
+macro_rules! define_fn {
+    ($f: ident, $ret: ty, $val: expr) => {
+        fn $f() -> $ret { $val }
+    };
+}
+
+#[test]
+fn test_define_fn() {
+    define_fn!(six, i32, 1 + 2 + 3);
+    assert_eq!(six(), 6);
+
+    define_fn!(hello, String, "hello".to_string());
+    assert_eq!(hello(), "hello");
+}
