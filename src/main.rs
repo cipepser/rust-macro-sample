93b2323236b2dacebe8a672788de54e307cf3851
cipepser <respepic@gmail.com>
2019-06-01 18:11:36 +0900

    add patterns

--- src/main.rs
+++ src/main.rs
@@ -8,3 +8,16 @@ macro_rules! basic {
 fn test_basic() {
     assert_eq!(basic!(), "basic");
 }
+
+macro_rules! patterns {
+    (cat) => { "cat" };
+    (dog) => { "dog" };
+    ({}) => { "brace" };
+}
+
+#[test]
+fn test_patterns() {
+    assert_eq!(patterns!(cat), "cat");
+    assert_eq!(patterns!(dog), "dog");
+    assert_eq!(patterns!({}), "brace");
+}
